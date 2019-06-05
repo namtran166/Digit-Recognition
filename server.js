5196144796f2b8ed88fbd569afc01be7542cbbf7
namtran166 <48459680+namtran166@users.noreply.github.com>
2019-06-05 11:38:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
 // All we need
+let express = require('express');
+let app = express();
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var port = process.env.port || 3000;
-var path = require('path');
-
-
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-// BodyParser configure
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
-app.use(bodyParser.json());
-
-// Setup templating engine
-app.set('view engine', 'ejs'); // setup EJS for templating
-
-
-// Adding routes
-require('./app/routes.js')(app);
-app.listen(port);
-
-console.log('The magic happens on port '+port);
+// Serve files from the static directory
+app.use(express.static('./static'));
+app.listen(3000, () => console.log('Server running on PORT 3000'));
+console.log("Please go to http://localhost:3000/index.html to see the magic :D");
